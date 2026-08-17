8124b08e94fce6febd27155c451ef1d46faea91a
Крылов Алексей <kraldison@MacBook-Air-Krylov.local>
2026-08-17 20:22:02 +0300

    Решил некоторые задачки 2 главы

---
+++ Part_2/cases/number_8.py
@@ -0,0 +1,34 @@
+#Упражнение 2.8
+
+print(5 + 3)
+print(4 * 2)
+print(8 / 1)
+print(10 - 2)
+
+#Упражнение 2.9
+
+favorite_number = 10
+message = f"My favorite number is {favorite_number}"
+print(message)
+
+#imort this
+
+#Beautiful is better than ugly.
+#Explicit is better than implicit.
+#Simple is better than complex.
+#Complex is better than complicated.
+#Flat is better than nested.
+#Sparse is better than dense.
+#Readability counts.
+#Special cases aren't special enough to break the rules.
+#Although practicality beats purity.
+#Errors should never pass silently.
+#Unless explicitly silenced.
+#In the face of ambiguity, refuse the temptation to guess.
+#There should be one-- and preferably only one --obvious way to do it.
+#Although that way may not be obvious at first unless you're Dutch.
+#Now is better than never.
+#Although never is often better than *right* now.
+#If the implementation is hard to explain, it's a bad idea.
+#If the implementation is easy to explain, it may be a good idea.
+#Namespaces are one honking great idea -- let's do more of those!
