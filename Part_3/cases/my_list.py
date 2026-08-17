2bc7a79927624c3fa39f145bf10c302fdde13132
Крылов Алексей <kraldison@MacBook-Air-Krylov.local>
2026-08-17 20:24:09 +0300

    Добавил папку с задачками главы 3

---
+++ Part_3/cases/my_list.py
@@ -0,0 +1,6 @@
+favorite_transport = ['Ниссан', 'Хонда', 'Тойота', 'Феррари']
+
+print(f'Я хотел бы подрифтить на {favorite_transport[0]}')
+print(f'Я хотел бы погонять на {favorite_transport[1]}')
+print(f'Я хотел бы купить {favorite_transport[3]}')
+print(f'Я хотел бы затюнить {favorite_transport[2]}')
---
+++ Part_3/cases/names.py
@@ -0,0 +1,16 @@
+#Создадим список имен моих друзей и каждый выведем
+
+names = ['Pasha', 'Vitya', 'Valya', 'Nika', 'Sasha']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+#Сейчас обратимся к тому же списку и выведем с каждым именем сообщения
+
+print(f"\nMy friend is {names[0]}")
+print(f"My friend is {names[1]}")
+print(f"My friend is {names[2]}")
+print(f"My friend is {names[3]}")
+print(f"My friend is {names[4]}")
