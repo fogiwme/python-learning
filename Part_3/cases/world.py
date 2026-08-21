eb950496fa69e140d1c88ef120f8ddb64cc415a3
Крылов Алексей <kraldison@MacBook-Air-Krylov.local>
2026-08-21 04:49:07 +0300

    Решил задачку из главы

---
+++ Part_3/cases/world.py
@@ -0,0 +1,22 @@
+#Упражнение 3.8
+
+countries = ['Japaneese', 'Canada', 'New Zeland', 'England', 'China']
+print(countries)
+print(sorted(countries))
+print(countries)
+
+print(f'\n')
+print(sorted(countries, reverse = True))
+print(countries)
+
+print(f'\n')
+countries.reverse()
+print(countries)
+countries.reverse()
+print(countries)
+
+print(f'\n')
+countries.sort()
+print(countries)
+countries.sort(reverse = True)
+print(countries)
