ded6cc0183f4bb8b99d797ef9782e46230726ed4
Крылов Алексей <kraldison@MacBook-Air-Krylov.local>
2026-08-26 23:23:05 +0300

    Решал новые кейсы

--- Part_4/cases/pizza.py
+++ Part_4/cases/pizza.py
@@ -1,9 +1,21 @@
 #Выполним упражнение 4.1
 
-pizzas = ['pepperoni pizza', 'margherita pizza', 'cheese pizza', 'hawaiian pizza']
+pizzas = ['pepperoni pizza', 'margherita pizza', 'cheese pizza']
 
 for pizza in pizzas:
 	print(f"I like {pizza.capitalize()}") #capitalize() делает букву первого слова в элементе списка заглавной, остальные не трогает
 
 print('\nI really love pizza!\n')
 
+
+friend_pizzas = pizzas[:]
+pizzas.append('just pizza')
+friend_pizzas.append('hawaiian pizza')
+
+print(f'\nMy favorite pizzas are:')
+for pizza in pizzas:
+	print(pizza.capitalize())
+
+print(f"\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+	print(pizza.capitalize())
