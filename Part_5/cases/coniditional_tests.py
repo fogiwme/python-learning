a271c44ac21b9d1e6dd6959ba26f129f9d0be0bc
Крылов Алексей <kraldison@MacBook-Air-Krylov.local>
2026-08-30 07:24:14 +0300

    Решал задачки к главе

---
+++ Part_5/cases/coniditional_tests.py
@@ -0,0 +1,55 @@
+#Выполним упражнение 5.1 
+
+car = 'subaru'
+car_2 = 'mazda'
+
+print(car == 'subaru')
+print(car == 'mazda')
+print(car != 'mazda')
+print(car != 'subaru')
+print(car_2 == 'subaru')
+print(car_2 == 'mazda')
+print(car_2 != 'subaru')
+print(car_2 != 'mazda')
+print('\n')
+print(car == 'subaru' or car_2 == 'subaru')
+print(car == 'mazda' or car_2 == 'subaru')
+print('\n')
+
+#Выполним упражнение 5.2
+
+word = 'one'
+print(word == 'one')
+print(word != 'one')
+print(word.lower() != 'One')
+print(word.lower() == 'One')
+print('\n')
+
+number = 5
+print(number > 10)
+print(number < 10)
+print(number <= 10)
+print(number >= 10)
+print('\n')
+
+print((word == "one") and (number == 5))
+print((word != "one") or (number == 5))
+print('\n')
+
+cars = ['mazda', 'toyota']
+print('mazda' in cars)
+print('toyota' not in cars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
